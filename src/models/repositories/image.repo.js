76ba4493011bc73cb619bc20823611fd1b6fe900
ThoNV <thonv@ltsgroup.tech>
2023-09-25 18:47:05 +0700

    update

--- src/models/repositories/image.repo.js
+++ src/models/repositories/image.repo.js
@@ -1,4 +1,53 @@
-const queryImage = () => {};
+const imageModel = require('../image.model')
+const { Types } = require('mongoose');
+
+const queryImage = async ({
+  query,
+  limit = 1,
+  sort = {
+    _id: -1
+  },
+  next_cursor = null,
+  previous_cursor = null,
+}) => {
+  if (next_cursor) {
+    query['_id'] = { $lt: new Types.ObjectId(next_cursor) }
+  }
+
+  if (previous_cursor) {
+    query['_id'] = { $gt: new Types.ObjectId(previous_cursor) }
+  }
+
+
+  const data = await imageModel.find(query).sort(sort).limit(limit)
+
+  let lastItemCursor, firstItemCursor;
+
+  if (data.length) {
+    const lastItemId = data[data.length - 1]._id;
+    const firstItemId = data[0]._id;
+
+    const queryNextItem = { _id: { $lt: lastItemId } };
+    const queryPrevItem = { _id: { $gt: firstItemId } }
+    const data1 = await Promise.all([imageModel.findOne(queryNextItem), imageModel.findOne(queryPrevItem)]);
+
+    console.log('data1: ', data1);
+
+    // if (result1) {
+    //   lastItemCursor = lastItemId
+    // }
+    // if (result2) {
+    //   firstItemCursor = firstItemId
+    // }
+
+  }
+
+  return {
+    data,
+    lastItemCursor,
+    firstItemCursor
+  }
+};
 
 module.exports = {
   queryImage,
--- src/services/image.service.js
+++ src/services/image.service.js
@@ -2,6 +2,7 @@ const imageModel = require('../models/image.model');
 const { uploadFile } = require('../helpers/awsS3');
 const { Types } = require('mongoose');
 const { BadRequestError } = require('../core/error.response');
+const { queryImage } = require('../models/repositories/image.repo')
 
 class ImageService {
   static async createImage(payload) {
@@ -31,27 +32,42 @@ class ImageService {
     return newImage;
   }
 
-  static async getImages({ image_shopId, next_cursor, previous_cursor }) {
-    const objQuery = {
+  static async getImages({ image_shopId, next_cursor, previous_cursor, belong }) {
+    const objQueryShopImages = {
       image_shopId,
+      // belong: 'shop'
     };
 
+    const { data: data1,
+      lastItemCursor,
+      firstItemCursor } = await queryImage({
+        query: objQueryShopImages,
+        next_cursor,
+        previous_cursor
+      })
+
+    console.log({
+      data1,
+      lastItemCursor,
+      firstItemCursor
+    });
+
     if (next_cursor) {
-      objQuery._id = { $lt: new Types.ObjectId(next_cursor) };
+      objQueryShopImages._id = { $lt: new Types.ObjectId(next_cursor) };
     }
 
     if (previous_cursor) {
-      objQuery._id = {
+      objQueryShopImages._id = {
         $gt: new Types.ObjectId(previous_cursor),
       };
     }
 
     const data = await imageModel
-      .find(objQuery)
+      .find(objQueryShopImages)
       .sort({
         _id: -1,
       })
-      .limit(1);
+      .limit(2);
 
     let hasNext, hasPrev, lastItem, firstItem;
 
